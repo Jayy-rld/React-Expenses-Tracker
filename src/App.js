c5b7f1931572c71bf326e7c8695a03ff0a273062
Joshua Famiroju <108738244+ja-rrz@users.noreply.github.com>
2022-10-01 20:46:03 +0100

    Updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import Expense from "./components/Expense";
-import ExpenseItem from "./components/ExpenseItem";
+import React from 'react';
 
-function App() {
+import NewExpense from './components/NewExpense/NewExpense';
+import Expenses from './components/Expenses/Expenses';
+
+const App = () => {
   const expenses = [
     {
       id: 'e1',
@@ -9,10 +11,7 @@ function App() {
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
-    { id: 'e2',
-     title: 'New TV',
-      amount: 799.49,
-      date: new Date(2021, 2, 12) },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
     {
       id: 'e3',
       title: 'Car Insurance',
@@ -25,13 +24,24 @@ function App() {
       amount: 450,
       date: new Date(2021, 5, 12),
     },
-  ]
- 
+  ];
+
+  const addExpenseHandler = expense => {
+    console.log('In App.js');
+    console.log(expense);
+  };
+
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
+
   return (
     <div>
-     <p>Getting Started...</p>
-      <Expense items={expenses}></Expense> 
-    
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
     </div>
   );
 }
